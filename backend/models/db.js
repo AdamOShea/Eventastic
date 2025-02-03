1ea87b4a9da6079b4c86e5ec6b97a02b8d509edc
Adam O'Shea <C20372181@mytudublin.ie>
2025-02-03 14:43:35 +0000

    v0.2.19

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg'); 
+require('dotenv').config();
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
-  password: 'postgres',
-  port: 5432,
+  user: process.env.DB_USERNAME,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PW,
+  port: process.env.DB_PORT,
 });
 
 module.exports = { pool }; 
