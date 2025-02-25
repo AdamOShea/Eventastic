7974676d9150c14f0de7b77391c6cd957f59de31
Adam O'Shea <C20372181@mytudublin.ie>
2025-02-25 14:24:17 +0000

    v0.2.50 tm

--- backend/scrapersAndApis/ticketmasterAPI.js
+++ backend/scrapersAndApis/ticketmasterAPI.js
@@ -22,7 +22,7 @@ const ticketmasterAPI = async (keyword) => {
 
     const insertQuery = `
       INSERT INTO eventastic."Event" (venue, eventlocation, seller, date, time, artist, eventtype, genre, price, eventlink, title)
-      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
       ON CONFLICT (eventlink) DO NOTHING;
     `;
 
