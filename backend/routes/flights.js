e40ade6a2f2c7a83fb91ff539d5a56e191f31a8f
Adam O'Shea <C20372181@mytudublin.ie>
2025-03-10 23:14:47 +0000

    v0.3.16 flights backend

--- backend/routes/flights.js
+++ backend/routes/flights.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { flightsApis, detectAPIs } = require('../controllers/flights');
 
-router.post('/accomm-apis', flightsApis);
+router.post('/flights-apis', flightsApis);
 router.post('/detect-apis', detectAPIs);
 
 module.exports = router;
