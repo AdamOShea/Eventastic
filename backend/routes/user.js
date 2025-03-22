15cf33762af78758b2ee4d35a49a0b59c1c3422f
Adam O'Shea <C20372181@mytudublin.ie>
2025-03-22 14:02:31 +0000

    v0.6.1 some friends backend

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const { userValidation, validateRegister, validateLogin } = require('../middlewares/user.js');
-const { createUser, loginUser, findOneUser } = require('../controllers/user.js');
+const { createUser, loginUser, findOneUser, searchUsers } = require('../controllers/user.js');
 
 router.post('/create-user', validateRegister, userValidation, createUser);
 router.post('/login-user', validateLogin, userValidation, loginUser);
 router.post('/find-one-user', findOneUser);
+router.post('/search-users', searchUsers);
 
 module.exports = router; 
